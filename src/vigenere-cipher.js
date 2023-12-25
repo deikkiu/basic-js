cc9bed34b9a74819627e9c9d1b05d3ab2b75d433
Timur Smagulov <deikku@mail.ru>
2023-12-26 00:28:23 +0600

    feat: completed basic js tasks

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,35 +1,83 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require('../extensions/index.js')
 
 /**
  * Implement class VigenereCipheringMachine that allows us to create
  * direct and reverse ciphering machines according to task description
- * 
+ *
  * @example
- * 
+ *
  * const directMachine = new VigenereCipheringMachine();
- * 
+ *
  * const reverseMachine = new VigenereCipheringMachine(false);
- * 
+ *
  * directMachine.encrypt('attack at dawn!', 'alphonse') => 'AEIHQX SX DLLU!'
- * 
+ *
  * directMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => 'ATTACK AT DAWN!'
- * 
+ *
  * reverseMachine.encrypt('attack at dawn!', 'alphonse') => '!ULLD XS XQHIEA'
- * 
+ *
  * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
- * 
+ *
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+	constructor(direct = true) {
+		this.direct = direct
+	}
+
+	encrypt(message, key) {
+		if (message === undefined || key === undefined) {
+			throw new Error('Incorrect arguments!')
+		}
+		message = message.toUpperCase()
+		key = key.toUpperCase()
+		let result = ''
+		let keyIndex = 0
+		for (let i = 0; i < message.length; i++) {
+			if (65 <= message.charCodeAt(i) && message.charCodeAt(i) < 91) {
+				result += String.fromCharCode(
+					((message.charCodeAt(i) -
+						65 +
+						key.charCodeAt(keyIndex % key.length) -
+						65) %
+						26) +
+						65
+				)
+				keyIndex++
+			} else {
+				result += message[i]
+			}
+		}
+		return this.direct ? result : result.split('').reverse().join('')
+	}
+
+	decrypt(encryptedMessage, key) {
+		if (encryptedMessage === undefined || key === undefined) {
+			throw new Error('Incorrect arguments!')
+		}
+		key = key.toUpperCase()
+		let result = ''
+		let keyIndex = 0
+		for (let i = 0; i < encryptedMessage.length; i++) {
+			if (
+				65 <= encryptedMessage.charCodeAt(i) &&
+				encryptedMessage.charCodeAt(i) < 91
+			) {
+				result += String.fromCharCode(
+					((encryptedMessage.charCodeAt(i) +
+						26 -
+						key.charCodeAt(keyIndex % key.length)) %
+						26) +
+						65
+				)
+				keyIndex++
+			} else {
+				result += encryptedMessage[i]
+			}
+		}
+		return this.direct ? result : result.split('').reverse().join('')
+	}
 }
 
 module.exports = {
-  VigenereCipheringMachine
-};
+	VigenereCipheringMachine
+}
--- src/what-season.js
+++ src/what-season.js
@@ -1,21 +1,38 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require('../extensions/index.js')
 
 /**
  * Extract season from given date and expose the enemy scout!
- * 
+ *
  * @param {Date | FakeDate} date real or fake date
  * @returns {String} time of the year
- * 
+ *
  * @example
- * 
+ *
  * getSeason(new Date(2020, 02, 31)) => 'spring'
- * 
+ *
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getSeason(date) {
+	const INCORRECT_RESULT_MSG = 'Invalid date!'
+
+	if (!date) return 'Unable to determine the time of year!'
+
+	// Проверка на объект типа Date
+	if (!(date instanceof Date) || Object.keys(date).length > 0) {
+		throw new Error(INCORRECT_RESULT_MSG)
+	}
+
+	const month = date.getMonth()
+
+	if (isNaN(month)) {
+		throw new Error(INCORRECT_RESULT_MSG)
+	}
+
+	if (month < 2 || month === 11) return 'winter'
+	if (month < 5) return 'spring'
+	if (month < 8) return 'summer'
+	return 'autumn'
 }
 
 module.exports = {
-  getSeason
-};
+	getSeason
+}
